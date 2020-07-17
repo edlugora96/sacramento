2c9b3cf3edb6cabd256dabd87775d54d55799257
Eduardo Gonzalez <edlu.gora96@gmail.com>
2020-07-17 15:53:00 -0400

    Continuous integration with Travis CI - Setting up

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+HOST=$1
+USER=$2
+PASSWD=$3
+LOCALPATH='public/'
+FILE='*'
+DIR='.'
+
+ftp -n $HOST <<EOF
+quote USER $USER
+quote PASS $PASSWD
+cd $DIR
+lcd $LOCALPATH
+mput $FILE
+quit
+exit;
+EOF
