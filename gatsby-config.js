79707f9c554159edd9ad78aed80d8ea65f8c770a
Eduardo Gonzalez <edlu.gora96@gmail.com>
2020-07-17 08:26:27 -0400

    Start

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `3439Sacramento`,
+    description: `3439Sacramento new listing.`,
+    author: `@edlugora`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `KirkAndPeggy`,
+        short_name: `3439Sacramento`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>New listing</h1>
+    <p>Welcome to your new site 3439Sacramento.</p>
+  </Layout>
+)
+
+export default IndexPage
